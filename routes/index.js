939eb5b7b594e85887e17dfe039e0d4a40c235b7
lkysammi <kleun136@my.centennialcollege.ca>
2023-06-07 19:45:50 -0400

    project created

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET IBM page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'IBM' });
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'Home' });
+});
+
+/* GET About me page. */
+router.get('/aboutme', function(req, res, next) {
+  res.render('aboutme', { title: 'About Me' });
+});
+
+/* GET Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'My Projects' });
+});
+
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'My Services' });
+});
+
+/* GET Contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Me' });
+});
+
+
+module.exports = router;
